bb9af1fb3b3b6911e70ce5424c245275f118b773
samir <samirsamawade@gmail.com>
2022-04-18 16:50:31 +0300

    First Commit

---
+++ app/src/main/java/com/samawade/loginandsignupwithmvvmretrofit/auth/RegisterFragment.kt
@@ -0,0 +1,12 @@
+package com.samawade.loginandsignupwithmvvmretrofit.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.samawade.loginandsignupwithmvvmretrofit.R
+
+class RegisterFragment : Fragment() {
+
+}
