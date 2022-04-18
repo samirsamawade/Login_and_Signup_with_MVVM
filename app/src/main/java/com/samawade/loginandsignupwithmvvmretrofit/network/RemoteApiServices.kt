d360d19fed4e9a5beee55dd80a2f1e80541885b8
samir <samirsamawade@gmail.com>
2022-04-18 17:54:25 +0300

    Reyrofit files

---
+++ app/src/main/java/com/samawade/loginandsignupwithmvvmretrofit/network/RemoteApiServices.kt
@@ -0,0 +1,20 @@
+package com.samawade.loginandsignupwithmvvmretrofit.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RemoteApiServices {
+    companion object{
+        private const val BASE_URL = "https://simplifiedcoding.tech/mywebapp/public/api/"
+    }
+
+    fun <Api> buildApi(
+        api: Class<Api>
+    ): Api {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(api)
+    }
+}
---
+++ app/src/main/java/com/samawade/loginandsignupwithmvvmretrofit/network/Resource.kt
@@ -0,0 +1,12 @@
+package com.samawade.loginandsignupwithmvvmretrofit.network
+
+import okhttp3.ResponseBody
+
+sealed class Resource<out T> {
+    data class Success<out T> (val value: T): Resource<T>()
+    data class Failure(
+        val isNetworkError: Boolean,
+        val errorCode: Int?,
+        val errorBody: ResponseBody?
+    )
+}
