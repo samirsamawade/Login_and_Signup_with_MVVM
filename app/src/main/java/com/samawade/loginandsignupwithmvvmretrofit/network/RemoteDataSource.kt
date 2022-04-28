794ef29f6f190fa69cacb0829850df8e60fef720
samir <samirsamawade@gmail.com>
2022-04-28 14:42:06 +0300

    Register Fragment UI Updated

--- app/src/main/java/com/samawade/loginandsignupwithmvvmretrofit/network/RemoteDataSource.kt
+++ app/src/main/java/com/samawade/loginandsignupwithmvvmretrofit/network/RemoteDataSource.kt
@@ -14,10 +14,11 @@ class RemoteDataSource {
     fun <Api> buildApi(
         api: Class<Api>
     ): Api {
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(
-                OkHttpClient.Builder().also { client ->
+                OkHttpClient.Builder().also { client->
                     if (BuildConfig.DEBUG) {
                         val logging = HttpLoggingInterceptor()
                         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
--- app/src/main/java/com/samawade/loginandsignupwithmvvmretrofit/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/samawade/loginandsignupwithmvvmretrofit/ui/auth/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.samawade.loginandsignupwithmvvmretrofit.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,9 +24,11 @@ class LoginFragment : BaseFragment<AuthViewModel, FragmentLoginBinding, AuthRepo
             when(it){
                 is Resource.Success -> {
                     Toast.makeText(requireContext(), it.toString(), Toast.LENGTH_LONG).show()
+                    Log.d("TAG", it.toString())
                 }
                 is Resource.Failure -> {
                     Toast.makeText(requireContext(), "Login Failure", Toast.LENGTH_LONG).show()
+                    Log.d("TAG", it.toString())
                 }
             }
         })
